5453d26545a8c7fd18b2f57ed2e2145ba9696c4d
gemer <eduar_pesc@hotmail.com>
2023-05-06 12:00:24 -0600

    Generate base Project

---
+++ src/main/java/Conversor/App.java
@@ -0,0 +1,13 @@
+package Conversor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
